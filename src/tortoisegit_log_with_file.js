d5238fe5ec15f63279be497ad17668ab7cd9f167
s_hiiragi <hiiragi.lab@gmail.com>
2016-10-14 03:50:27 +0900

    add tortoisegit_log_with_xxx.js (xxx=file/folder)

---
+++ src/tortoisegit_log_with_file.js
@@ -0,0 +1,25 @@
+/**
+ * @file  ファイルを指定してTortoiseGitのログ画面を開く
+ */
+(function(){
+	var docPath = Editor.GetFilename();
+	if (!docPath) return;
+
+	var fso = new ActiveXObject('Scripting.FileSystemObject');
+	var docFolderPath = fso.GetParentFolderName(docPath);
+	
+	var tortoiseGitProcPath = "C:\\Program Files\\TortoiseGit\\bin\\TortoiseGitProc.exe";
+	if (!fso.FileExists(tortoiseGitProcPath)) {
+		Editor.ErrorMsg(tortoiseGitProcPath + 'が見つかりません');
+		return;
+	}
+	
+	var command = '"' + tortoiseGitProcPath + '"'
+		+ ' /command:log'
+		+ ' /path:"' + docPath + '"';
+
+//	Editor.TraceOut('command=' + command);
+
+	var sh = new ActiveXObject('WScript.Shell');
+	sh.Run(command);
+})();
