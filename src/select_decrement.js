1597f4fa153a9a149f02f2879f1c7f540699db4c
s_hiiragi <hiiragi.lab@gmail.com>
2021-06-14 00:28:36 +0900

    merge lib folder to modules folder

--- src/select_decrement.js
+++ src/select_decrement.js
@@ -1,6 +1,6 @@
 eval(function(){
 	var fso = new ActiveXObject('Scripting.FileSystemObject');
-	var src = fso.OpenTextFile('lib/rotateSelectedText.js').ReadAll();
+	var src = fso.OpenTextFile('modules/rotateSelectedText.js').ReadAll();
 	return src;
 }());
 
