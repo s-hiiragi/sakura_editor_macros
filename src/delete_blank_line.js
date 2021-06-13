0f27253c1102399bb8030df13e28c0f2f2a1f948
s_hiiragi <hiiragi.lab@gmail.com>
2021-06-14 00:29:28 +0900

    add macros

---
+++ src/delete_blank_line.js
@@ -0,0 +1,16 @@
+﻿/*
+ * 空行を詰める
+ */
+
+var n = Editor.GetLineCount(0);
+
+for (var i = 1; i <= n; ) {
+    Editor.MoveCursor(i, 1, 0);
+    var line = Editor.GetLineStr(i).replace(/^\s+|\s+$/g, '');
+    if (line === '') {
+        Editor.Delete();
+        n = Editor.GetLineCount(0);
+        continue;
+    }
+    i++;
+}
---
+++ src/insert_heading_number.js
@@ -0,0 +1,36 @@
+﻿/*
+ * 選択範囲の各行に見出し番号(連番)を挿入する
+ */
+
+function calcKeta(n) {
+    var keta = 1;
+    while (n >= 10) {
+        keta++;
+        n /= 10;
+    }
+    return keta;
+}
+
+function repeatString(s, n) {
+    var result = '';
+    for (var i = 0; i < n; i++) {
+        result += s;
+    }
+    return result;
+}
+
+var startLine = Editor.GetSelectLineFrom();
+var endLine = Editor.GetSelectLineTo();
+
+var maxNum = endLine - startLine + 1;
+var keta = calcKeta(maxNum);
+var zeros = repeatString('0', keta);
+
+AddRefUndoBuffer();
+
+for (var i = 0; i < maxNum; i++) {
+    Editor.MoveCursor(startLine + i, 1, 0);
+    Editor.InsText((zeros + String(i + 1)).slice(-keta) + '. ');
+}
+
+SetUndoBuffer();
