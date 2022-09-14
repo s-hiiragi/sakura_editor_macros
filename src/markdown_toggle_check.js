ec779f030dadb5d1f20140145737d1f927222663
s_hiiragi <hiiragi.lab@gmail.com>
2022-09-15 06:43:41 +0900

    マクロを追加

---
+++ src/markdown_toggle_check.js
@@ -0,0 +1,27 @@
+﻿/*
+ * (Markdown) チェックボックスの状態を変更する
+ *
+ * 参考
+ *
+ * - [5.3 Task list items (extension)](https://github.github.com/gfm/#task-list-items-extension-)
+ */
+
+(function(){
+    var newLine = ['\r\n', '\r', '\n'][Editor.GetLineCode()];
+
+    var currentLine = Editor.GetLineStr(0).replace(/\r?\n$/, '');
+
+    var m = /^(\s*-\s*\[)([ xX])(\] .*)/.exec(currentLine);
+    if (!m) { return; }
+
+    var replacedLine = m[1] + (m[2] === ' ' ? 'x' : ' ') + m[3] + newLine;
+
+    var x = Editor.ExpandParameter('$x');
+    var y = Editor.ExpandParameter('$y');
+
+    Editor.SelectLine(0);
+    Editor.InsText(replacedLine);
+
+    // カーソルが改行の後ろに移動するので元に戻す
+    Editor.MoveCursor(y, x, 0);
+})();
