a8d11b7c9d50d766e66b537656d18d8e55b16808
s_hiiragi <hiiragi.lab@gmail.com>
2022-09-15 07:39:41 +0900

    マクロを追加

---
+++ src/markdown_cut_section.js
@@ -0,0 +1,52 @@
+﻿/*
+ * (Markdown) カーソル位置の見出しと本文を切り取る
+ */
+
+(function(){
+    var n = Editor.GetLineCount(0);
+    var y = Number(Editor.ExpandParameter('$y'));
+
+    // 見出しを探す
+
+    var i = y;
+    var headerRow = null;
+
+    for (; i >= 1; i--) {
+        var line = Editor.GetLineStr(i);
+
+        if (/^#+ /.test(line)) {
+            headerRow = i;
+            break;
+        }
+    }
+
+    if (headerRow === null) {
+        Editor.MsgBeep(4);
+        return;
+    }
+
+    // 次の見出しを探す
+
+    var i = y + 1;
+    var nextHeaderRow = null;
+
+    for (; i <= n; i++) {
+        var line = Editor.GetLineStr(i);
+
+        if (/^#+ /.test(line)) {
+            nextHeaderRow = i;
+            break;
+        }
+    }
+
+    if (nextHeaderRow === null) {
+        nextHeaderRow = n;
+    }
+
+    Editor.MoveCursor(headerRow, 1, 0);
+    Editor.MoveCursor(nextHeaderRow, 1, 1);
+    if (nextHeaderRow === n) {
+        Editor.GoLineEnd_Sel();
+    }
+    Editor.Cut();
+})();
---
+++ src/markdown_goto_next_header.js
@@ -0,0 +1,31 @@
+﻿/*
+ * (Markdown) 次の見出しに移動
+ *
+ * NOTE
+ *
+ * - コードブロック中の`#`で始まる行にもマッチしてしまうが、
+ *   現在行がコードブロック中かどうか確実に判定する方法が無いため、妥協する。
+ */
+
+(function(){
+    var n = Editor.GetLineCount(0);
+    var i = Number(Editor.ExpandParameter('$y')) + 1;
+    var headerRow = null;
+
+    for (; i <= n; i++) {
+        var line = Editor.GetLineStr(i);
+
+        if (/^#+ /.test(line)) {
+            headerRow = i;
+            break;
+        }
+    }
+
+    if (headerRow === null) {
+        Editor.MsgBeep(4);
+        return;
+    }
+
+    var x = Number(Editor.ExpandParameter('$x'));
+    Editor.MoveCursor(headerRow, x, 0);
+})();
