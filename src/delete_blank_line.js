124f64402383cd681f6529fb141af481aeab71d3
s_hiiragi <hiiragi.lab@gmail.com>
2022-09-15 07:41:57 +0900

    アンドゥ可能にした

--- src/delete_blank_line.js
+++ src/delete_blank_line.js
@@ -2,11 +2,14 @@
  * 空行を詰める
  */
 
+Editor.AddRefUndoBuffer();
+
 var n = Editor.GetLineCount(0);
 
-for (var i = 1; i <= n; ) {
+for (var i = 1; i <= n; )
+{
     Editor.MoveCursor(i, 1, 0);
-    var line = Editor.GetLineStr(i).replace(/^\s+|\s+$/g, '');
+    var line = Editor.GetLineStr(i).replace(/\r?\n$/, '');
     if (line === '') {
         Editor.Delete();
         n = Editor.GetLineCount(0);
@@ -14,3 +17,5 @@ for (var i = 1; i <= n; ) {
     }
     i++;
 }
+
+Editor.SetUndoBuffer();
