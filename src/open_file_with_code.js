9a882fa9b96bb58c5f4337c11f0f435c0e36b068
s_hiiragi <hiiragi.lab@gmail.com>
2023-10-30 03:33:03 +0900

    マクロを追加

---
+++ src/open_file_with_code.js
@@ -0,0 +1,9 @@
+﻿/*
+ * ファイルをVS Codeで開く
+ */
+
+var codePath = 'C:\\Program Files\\Microsoft VS Code\\bin\\code.cmd';
+var docPath = Editor.ExpandParameter('$F');
+var cmd = '"' + codePath + '" "' + docPath + '"';
+
+Editor.ExecCommand(cmd, 0);
