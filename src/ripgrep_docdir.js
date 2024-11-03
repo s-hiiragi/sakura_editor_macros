cef7576121350f26f9928a179585bda8033bb824
s_hiiragi <hiiragi.lab@gmail.com>
2024-11-03 23:12:57 +0900

    add ripgrep_docdir.js

---
+++ src/ripgrep_docdir.js
@@ -0,0 +1,19 @@
+﻿/**
+ * @file  ドキュメントのあるディレクトリをrgコマンドで検索
+ */
+
+/*
+ * 課題
+ * - 行番号が出力されないのが致命的
+ */
+
+var dir = Editor.ExpandParameter('$e');
+var kwd = Editor.InputBox('検索ワード', '', 100);
+
+var opt = 0;
+opt |= 0x01;  // 標準出力を得る
+opt |= 0x80;  // 標準出力をUTF-8で行う
+
+if (kwd) {
+    Editor.ExecCommand('rg ' + kwd + ' ' + dir, opt);
+}
