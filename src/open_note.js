f143481c7cc5678934fd24d914033b3bf5bcd151
s_hiiragi <hiiragi.lab@gmail.com>
2025-02-04 21:06:42 +0900

    modify: open_note.jsでノートのエイリアス名を扱えるよう拡張

--- src/open_note.js
+++ src/open_note.js
@@ -5,13 +5,22 @@
  *
  * - 環境変数`SAKURA_NOTEPATH`に、ノート(.mdファイル)が格納されたフォルダのパスを設定する
  *
+ * 各ノートの先頭付近にエイリアス名を書くことで、ノートをエイリアス名で開くことができる
+ * 
+ * エイリアスの設定方法 (省略可)
+ * 
+ * - 各ノートの先頭付近に`alias: エイリアス名1, エイリアス名2, …`と書く
+ * - open_note_update_aliases.jsを実行する
+ *
  * 推奨ショートカットキー: Alt+N
  */
 
 /*
- * TODO ノートを別名で開きたい
  * TODO ファイル名に使えない文字を全角に変換したい
  */
+
+var fso = new ActiveXObject('Scripting.FileSystemObject');
+var wsh = new ActiveXObject('WScript.Shell');
 
 function trimExtension(path) {
     return path.replace(/\.[^\.]+$/, '');
@@ -21,9 +30,57 @@ function hasExtension(path) {
     return /\.[^.]+$/.test(path);
 }
 
+// @return {[string]: string}
+function readAliases(filepath) {
+    var aliases = {};
+
+    if (fso.FileExists(filepath)) {
+        var file = fso.OpenTextFile(filepath, 1, false, -1);  // Unicode
+        var lines = file.ReadAll().split(/\r?\n/);
+        file.Close();
+
+        var reading = false;
+        for (var i = 0; i < lines.length; i++) {
+            if (reading) {
+                if (lines[i].charAt(0) === '[') {
+                    break;
+                }
+                var pair = lines[i].split(/\s*=\s*/);
+                var key = pair[0];
+                var val = pair[1];
+                if (key && val) {
+                    aliases[key] = val;
+                }
+            }
+            else {
+                if (lines[i] === '[aliases]') {
+                    reading = true;
+                }
+            }
+        }
+    }
+
+    return aliases;
+}
+
+// @return {Path: string, Name: string}[]
+function getFiles(folderPath) {
+    var files = [];
+
+    var dir = fso.GetFolder(folderPath);
+
+    for (var e = new Enumerator(dir.Files); !e.atEnd(); e.moveNext()) {
+        var f = e.item();
+        files.push(f);
+    }
+
+    return files;
+}
+
 (function(){
 
-    var wsh = new ActiveXObject('WScript.Shell');
+    // ノートのあるディレクトリのパスを取得
+
     var noteDirPath = wsh.ExpandEnvironmentStrings('%SAKURA_NOTEPATH%');
 
 	if (noteDirPath.charAt(0) === '%') {
@@ -31,21 +88,31 @@ function hasExtension(path) {
 		return;
 	}
 
+	// 設定ファイルからノートの別名を読み込む
+
+    var settingsFilePath = 'settings\\open_note.ini';
+    var aliases = readAliases(settingsFilePath);
+
+    // 開くor作成するノート名を入力
+
     var noteName = Editor.InputBox('ノート名', '', 100);
     if (!noteName) return;
 
-    var files = [];
-    var fso = new ActiveXObject('Scripting.FileSystemObject');
+    // エイリアス名があれば置換する
 
-    var noteDir = fso.GetFolder(noteDirPath);
-    for (var e = new Enumerator(noteDir.Files); !e.atEnd(); e.moveNext()) {
-        var f = e.item();
-        files.push(f);
+    if (aliases[noteName]) {
+        noteName = aliases[noteName];
     }
+
+    // ノートファイル一覧を取得
+
+    var files = getFiles(noteDirPath);
+
+    // 入力したノート名に一致するノートファイルを探す
 
     var notePath = null;
 
-    // 完全一致を試す
+    // 1. 完全一致を試す
     // ただし、利便性のために、
     // - 大文字/小文字は区別しない
     // - 拡張子なしで比較する
@@ -57,7 +124,7 @@ function hasExtension(path) {
         }
     }
     if (!notePath) {
-        // 部分一致を試す
+        // 2. 部分一致を試す
         // ただし、
         // - 拡張子を指定されている場合は試さない
         if (!hasExtension(noteName)) {
@@ -70,6 +137,8 @@ function hasExtension(path) {
             }
         }
     }
+
+    // ノートを開く
 
     if (notePath) {
         Editor.FileOpen(notePath);
---
+++ src/open_note_update_aliases.js
@@ -0,0 +1,107 @@
+﻿/**
+ * @file ノートのエイリアス設定を更新する
+ *
+ * 事前準備
+ *
+ * - 環境変数`SAKURA_NOTEPATH`に、ノート(.mdファイル)が格納されたフォルダのパスを設定する
+ *
+ * 推奨ショートカットキー: Alt+N
+ */
+
+var fso = new ActiveXObject('Scripting.FileSystemObject');
+var wsh = new ActiveXObject('WScript.Shell');
+
+function trimExtension(filepath) {
+    return filepath.replace(/\.[^.]+$/, '');
+}
+
+// @return {Path: string, Name: string}[]
+function getFiles(folderPath) {
+    var files = [];
+
+    var dir = fso.GetFolder(folderPath);
+
+    for (var e = new Enumerator(dir.Files); !e.atEnd(); e.moveNext()) {
+        var f = e.item();
+        files.push(f);
+    }
+
+    return files;
+}
+
+// @return string[]
+function readAliasesFromNote(notePath) {
+    var aliases = [];
+
+    var stream = new ActiveXObject('ADODB.Stream');
+    stream.Charset = 'UTF-8';
+    stream.Open();
+    stream.LoadFromFile(notePath);
+    
+    for (var i = 0; i < 10; i++) {
+        if (stream.EOS) {
+            break;
+        }
+        var line = stream.ReadText(-2).replace(/\r?\n$/, '');  // adReadLine
+        if (/^alias:/.test(line)) {
+            aliases = line.replace(/^alias:\s*/, '').split(/\s*,\s*/);
+            break;
+        }
+    }
+    stream.Close();
+    
+    return aliases;
+}
+
+function writeAliases(settingFile, noteByAlias) {
+    var file = fso.OpenTextFile(settingFile, 2, true, -1);  // Unicode
+
+    file.WriteLine('[aliases]');
+
+    for (var alias in noteByAlias) {
+        file.WriteLine(alias + '=' + noteByAlias[alias]);
+    }
+
+    file.Close();
+}
+
+(function(){
+
+    // ノートのあるディレクトリのパスを取得
+
+    var noteDirPath = wsh.ExpandEnvironmentStrings('%SAKURA_NOTEPATH%');
+
+	if (noteDirPath.charAt(0) === '%') {
+		WarnMsg('環境変数 SAKURA_NOTEPATH を設定してください。');
+		return;
+	}
+
+    // ノートファイル一覧を取得
+
+    var files = getFiles(noteDirPath);
+
+    // 各ノートからエイリアスを読みだす
+    
+    var noteNameByAlias = {};
+    
+    for (var i = 0; i < files.length; i++) {
+        var f = files[i];
+        var name = trimExtension(f.Name.toLowerCase());
+        var aliases = readAliasesFromNote(f.Path);
+
+        for (var k = 0; k < aliases.length; k++) {
+            var alias = aliases[k];
+            noteNameByAlias[alias] = name;
+        }
+    }
+
+    for (var k in noteNameByAlias) {
+        traceout = k + '=' + noteNameByAlias[k];
+    }
+
+	// 設定ファイルにノートの別名を書き込む
+
+    var settingsFilePath = 'settings\\open_note.ini';
+    writeAliases(settingsFilePath, noteNameByAlias);
+
+})();
