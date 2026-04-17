52b2d19d562407fd83fd320cd946c31960a6efa7
s_hiiragi <hiiragi.lab@gmail.com>
2026-04-17 09:00:13 +0900

    add macro

---
+++ src/on_b_key_ex.js
@@ -0,0 +1,37 @@
+﻿/*
+ * on_b_key_ex.js
+ * bキー (拡張)
+ *
+ * 機能
+ *
+ * テキスト未選択時: 'b'を入力
+ * テキスト選択時: 選択範囲を'javascript:(()=>{ ... })()'で囲む (Bookmarklet)
+ *
+ * 使用方法
+ *
+ * - 共通設定＞マクロを開き、本ファイルを登録する
+ * - 共通設定＞キー割り当てを開き、本マクロを'J'キーに割り当てる
+ */
+
+switch (Editor.IsTextSelected()) {
+case 0: // 非選択状態
+case 2: // 矩形選択中
+    Editor.InsText('b');
+    break;
+case 1: // 選択中
+    var itext = Editor.GetSelectedString(0);
+    var otext;
+    if (/\n/.test(itext)) {
+        // 行選択中
+        var newline = ['\r\n', '\r', '\n'][Editor.GetLineCode()];
+        if (/\n$/.test(itext)) {
+            otext = 'javascript:(()=>{' + newline + itext.replace(/\r?\n$/, '') + newline + '})()' + newline;
+        } else {
+            otext = 'javascript:(()=>{' + newline + itext + newline + '})()';
+        }
+    } else {
+        otext = 'b';
+    }
+    Editor.InsText(otext);
+    break;
+}
---
+++ src/on_slash_key_ex.js
@@ -0,0 +1,41 @@
+﻿/*
+ * on_slash_key_ex.js
+ * '/'キー (拡張)
+ *
+ * 機能
+ *
+ * テキスト未選択時: '/'を入力
+ * テキスト選択時: 選択範囲の各行の行頭に'// 'を挿入 (C/C++ コメント)
+ *
+ * 使用方法
+ *
+ * - 共通設定＞マクロを開き、本ファイルを登録する
+ * - 共通設定＞キー割り当てを開き、本マクロを'/'キーに割り当てる
+ *
+ * TODO 再度実行したらアンコメントしたい
+ */
+
+switch (Editor.IsTextSelected()) {
+case 0: // 非選択状態
+case 2: // 矩形選択中
+    Editor.InsText('/');
+    break;
+case 1: // 選択中
+    var itext = Editor.GetSelectedString(0);
+    var otext;
+    if (/\n/.test(itext)) {
+        // 行選択中
+        var newline = ['\r\n', '\r', '\n'][Editor.GetLineCode()];
+        var prefix = '// ';
+        var lines = itext.replace(/\r\n/g, '\n').split('\n');
+        if (lines[lines.length-1] === '') {
+            otext = prefix + lines.slice(0, lines.length-1).join(newline + prefix) + newline;
+        } else {
+            otext = prefix + lines.join(newline + prefix);
+        }
+    } else {
+        otext = '/';
+    }
+    Editor.InsText(otext);
+    break;
+}
